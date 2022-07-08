ab139b450719802485e726020e3c87e7efa1cfae
Ayhan Kırıkcı <ayhankiririkci@gmail.com>
2022-07-08 04:00:29 +0300

    drop image file ui created

---
+++ scripts/main.js
@@ -0,0 +1,51 @@
+const dropArea = document.getElementById("dropSection")
+
+;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, preventDefaults, false)   
+  document.body.addEventListener(eventName, preventDefaults, false)
+})
+
+;['dragenter', 'dragover'].forEach(eventName => {
+  dropArea.addEventListener(eventName, highlightAdd, false)
+})
+
+;['dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, highlightRemove, false)
+  })
+
+dropArea.addEventListener('drop', handleDrop, false)
+
+function preventDefaults (e) {
+  e.preventDefault()
+  e.stopPropagation()
+}
+
+function highlightAdd(e) {
+  dropArea.classList.add('highlight')
+}
+
+function highlightRemove(e) {
+    dropArea.classList.remove('highlight')
+  }
+
+function handleDrop(e) {
+  var dt = e.dataTransfer
+  var files = dt.files
+
+  handleFiles(files)
+}
+
+function handleFiles(files) {
+  files = [...files]
+  files.forEach(previewFile)
+}
+
+function previewFile(file) {
+  let reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onloadend = function() {
+    let img = document.createElement('img')
+    img.src = reader.result
+    document.getElementById('uploaded-image').appendChild(img)
+  }
+}
