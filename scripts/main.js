a77b4e25a37f98dbf22a575448732feb8f0b0596
Ayhan Kırıkcı <ayhankiririkci@gmail.com>
2022-07-11 01:46:44 +0300

    image resizer function writed and action form added

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 const app = {
     selector: {
         dropArea: document.getElementById("dropSection"),
+        actionContainer: document.getElementById("actionContainer"),
+        uploadedImages: document.getElementById("uploadedImage")
     },
     actions: {
         highlightAdd: function () {
@@ -26,11 +28,50 @@ const app = {
                 let img = document.createElement('img')
                 img.src = reader.result
                 document.getElementById('uploadedImage').appendChild(img)
+                actionContainer.classList.remove('d-none')
             }
         },
         preventDefaults: function (e) {
             e.preventDefault()
             e.stopPropagation()
+        },
+        resizeImages: function (base64Str, maxWidth = 500, maxHeight = 500) {
+            return new Promise((resolve) => {
+                let img = new Image()
+                img.src = base64Str
+                img.onload = () => {
+                    let canvas = document.createElement('canvas')
+                    const MAX_WIDTH = maxWidth
+                    const MAX_HEIGHT = maxHeight
+                    let width = img.width
+                    let height = img.height
+
+                    if (width > height) {
+                        if (width > MAX_WIDTH) {
+                            height *= MAX_WIDTH / width
+                            width = MAX_WIDTH
+                        }
+                    } else {
+                        if (height > MAX_HEIGHT) {
+                            width *= MAX_HEIGHT / height
+                            height = MAX_HEIGHT
+                        }
+                    }
+                    canvas.width = width
+                    canvas.height = height
+                    let ctx = canvas.getContext('2d')
+                    ctx.drawImage(img, 0, 0, width, height)
+                    resolve(canvas.toDataURL())
+                }
+            })
+        },
+        execute: function() {
+            var images = Array.from(app.selector.uploadedImages.querySelectorAll('img'));
+            images.map((x) => {
+                app.actions.resizeImages(x.getAttribute('src'), 100, 100).then((result) => {
+                    console.log(result);
+                });
+            })
         }
     },
     init: function () {
